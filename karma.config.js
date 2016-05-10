1ae39c3f3aa4f37409c69d330fb84a1d6af7c92a
YouHan26 <youhan26@gmail.com>
2016-05-10 11:33:20 +0800

    add karma config and npm config

---
+++ karma.config.js
@@ -0,0 +1,43 @@
+/**
+ * Created by YouHan on 2016/5/10.
+ */
+var path = require('path');
+
+//TODO
+
+module.exports = function (config) {
+    config.set({
+        frameworks: ['mocha', 'sinon-chai'],
+        browsers: ['PhantomJS'],
+        reporters: ['spec', 'coverage'],
+        files: [
+            'tests/vuefire.spec.js'
+        ],
+        preprocessors: {
+            // 'tests/vuefire.spec.js': ['webpack', 'sourcemap']
+        },
+        client: {
+            mocha: {
+                timeout: 10000
+            }
+        },
+        webpack: {
+            // devtool: '#inline-source-map',
+            // module: {
+            //     loaders: [{
+            //         include: path.resolve(__dirname, 'src/vuefire.js'),
+            //         loader: 'istanbul-instrumenter'
+            //     }]
+            // }
+        },
+        webpackMiddleware: {
+            noInfo: true
+        },
+        coverageReporter: {
+            reporters: [
+                // {type: 'lcov', subdir: '.'},
+                // {type: 'text-summary'}
+            ]
+        }
+    })
+};
